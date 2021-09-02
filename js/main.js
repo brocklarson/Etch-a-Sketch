cad45ba43a230ae54a84c74ea9cc09c2666c80e4
brocklarson <brock.larson0725@gmail.com>
2021-09-01 19:34:17 -0600

    Add style to buttons and slider

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ const blackButton = document.getElementById('blackButton');
 const rainbowButton = document.getElementById('rainbowButton');
 const greyscaleButton = document.getElementById('greyscaleButton');
 const selectColorButton = document.getElementById('selectColorButton');
-let colorState = 'black';
+const selectColorButtonContainer = document.getElementById('selectColorButtonContainer');
+let colorMode = 'blackButton';
 
 function makeGrid(gridSize = 16) {
     removeAllChildNodes();
@@ -25,71 +26,81 @@ function removeAllChildNodes() {
     }
 }
 
-function getSliderValue() {
-    sliderValueText.innerText = `${slider.value} \u00D7 ${slider.value}`;
-    makeGrid(slider.value);
-}
-
-function fillInGrid(event) {
-    pixelColor = getColor(event);
-    event.target.style.setProperty('background-color', pixelColor);
-}
-
-function getColor(event) {
-    if (colorState === 'black') return 'black';
-    if (colorState === 'rainbow') return `#${Math.floor(Math.random()*16777215).toString(16)}`;
-    if (colorState === 'eraser') return 'transparent';
-    if (colorState === 'color') return selectColorButton.value;
-    if (colorState === 'greyscale') {
-        let filterValue = event.target.style.filter;
-        let brightnessValue;
-        if (filterValue === '') {
-            brightnessValue = 1.0;
-        } else {
-            brightnessValue = filterValue.substring(filterValue.indexOf('(') + 1, filterValue.indexOf(')'));
-        }
-        if (brightnessValue > 0) brightnessValue -= 0.1;
-        event.target.style.filter = `brightness(${brightnessValue})`;
-        return 'var(--screen-color)';
-    }
-}
-
 function clearGrid() {
     let cells = document.querySelectorAll('.grid-cell');
     for (i = 0; i < cells.length; i++) {
         cells[i].style.setProperty('background-color', 'transparent');
     }
+    clearButton.blur();
 }
-//DONT FILL IN GRID WHEN USING COLOR PICKER//
-//commit changes!!!//
+
+function fillInGrid(event) {
+    pixelColor = getColor();
+
+    if (event.target.className === 'grid-cell') {
+        if (colorMode !== 'greyscaleButton') {
+            event.target.style.setProperty('filter', 'brightness(1)');
+            event.target.style.setProperty('background-color', pixelColor);
+        } else {
+            let filterValue = event.target.style.filter;
+            let brightnessValue;
+            if (filterValue === '') {
+                brightnessValue = 1.0;
+            } else {
+                brightnessValue = filterValue.substring(filterValue.indexOf('(') + 1, filterValue.indexOf(')'));
+            }
+            if (brightnessValue > 0) brightnessValue -= 0.1;
+            event.target.style.setProperty('filter', `brightness(${brightnessValue})`);
+            event.target.style.setProperty('background-color', pixelColor);
+        }
+    }
+}
+
+function getColor() {
+    if (colorMode === 'blackButton') return 'black';
+    if (colorMode === 'rainbowButton') return `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    if (colorMode === 'eraserButton') return 'transparent';
+    if (colorMode === 'selectColorButton') return selectColorButton.value;
+    if (colorMode === 'greyscaleButton') return 'var(--screen-color)';
+}
+
+function getSliderValue() {
+    sliderValueText.innerText = `${slider.value}\u00D7${slider.value}`;
+    makeGrid(slider.value);
+}
+
+function setButtonBackground() {
+    selectColorButtonContainer.style.setProperty('background-color', selectColorButton.value);
+}
+
+function setMode(event) {
+    colorMode = event.target.id;
+    if (colorMode === "selectColorButton") {
+        setButtonFocus(selectColorButtonContainer);
+        return;
+    }
+    setButtonFocus(event.target);
+}
+
+function setButtonFocus(currentButton) {
+    let buttons = document.querySelectorAll('.buttons');
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('current');
+    }
+    currentButton.classList.add('current');
+}
+
+//SHAKE FOR CLEAR
+
 makeGrid(16);
 
 slider.oninput = getSliderValue;
+selectColorButton.onchange = setButtonBackground;
+
 gameGridContainer.addEventListener('mouseover', fillInGrid);
 clearButton.addEventListener('click', clearGrid);
-eraserButton.addEventListener('click', () => { colorState = 'eraser' });
-blackButton.addEventListener('click', () => { colorState = 'black' });
-rainbowButton.addEventListener('click', () => { colorState = 'rainbow' });
-greyscaleButton.addEventListener('click', () => { colorState = 'greyscale' });
-selectColorButton.addEventListener('click', () => { colorState = 'color' });
-
-
-
-/*
-//OBJECTIVE: Create Etch-A-Sketch that "colors in" when mouse hovers over
-//HIGH LEVEL FEATURES:
-    1) Choose pixel size (slider)
-    2) Clear Screen [Shake Tablet] (Button)
-
-    3) Select color (Button)
-        3a) Color Display window
-    4) Select black (Button)
-    5) Rainbow mode (Button)
-    6) Grey scale mode (Button)
-    
-
-
-
-    
-
-*/
+eraserButton.addEventListener('click', setMode);
+blackButton.addEventListener('click', setMode);
+rainbowButton.addEventListener('click', setMode);
+greyscaleButton.addEventListener('click', setMode);
+selectColorButton.addEventListener('click', setMode);
